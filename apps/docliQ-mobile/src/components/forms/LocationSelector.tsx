cbdebf3368ebd7c4fb6bfc9b8958f0f28ea3a58a
Ngan Pham <nganpham@Ngans-MacBook-Air.local>
2026-01-30 11:39:17 +0700

    Fix errors of changing app name

--- apps/docliQ-mobile/src/components/forms/LocationSelector.tsx
+++ apps/docliQ-mobile/src/components/forms/LocationSelector.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { IconMapPin, IconSearch } from '@tabler/icons-react'
-import { useTranslation } from 'react-i18next'
 import { RadiusSlider } from './RadiusSlider'
 import { SavedLocations } from '../display/SavedLocations'
 import { Button } from '../ui'
